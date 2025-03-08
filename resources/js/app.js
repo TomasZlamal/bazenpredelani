cc927031b9256b94222d2d1c07dac1406ddff0b9
Tomáš Zlámal <zlamal@tomaszlamal.com>
2025-03-08 17:30:16 +0100

    Added default upload

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
 import './bootstrap';
+document.addEventListener("DOMContentLoaded", () => {
+    // Mobile menu toggle
+    const mobileMenuButton = document.querySelector('button[aria-controls="mobile-menu"]');
+    const mobileMenuIcons = mobileMenuButton.querySelectorAll("svg");
+    const mobileMenu = document.getElementById("mobile-menu");
+    const nav = document.getElementById("nav");
+
+    mobileMenuButton.addEventListener("click", () => {
+        const isExpanded = mobileMenuButton.getAttribute("aria-expanded") === "true";
+        mobileMenuButton.setAttribute("aria-expanded", !isExpanded);
+
+        mobileMenuIcons.forEach((icon) => icon.classList.toggle("hidden"));
+
+        // Show/hide the mobile menu
+        if (mobileMenu) {
+            mobileMenu.classList.toggle("hidden");
+        }
+    });
+});
