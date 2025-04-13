cc856e88bd34feab4ed528c692ee9bae7ffecc93
Michal Škoula <michal@skoula.com>
2025-04-13 13:53:12 +0200

    Refactor routes

--- routes/web.php
+++ routes/web.php
@@ -2,36 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('homepage');
-});
-
-Route::get('/bazen', function () {
-    return view('bazen');
-});
-Route::get('/dostupnost', function () {
-    return view('dostupnost');
-});
-Route::get('/sauna', function () {
-    return view('sauna');
-});
-Route::get('/cenik', function () {
-    return view('cenik');
-});
-Route::get('/ubytovani', function () {
-    return view('ubytovani');
-});
-Route::get('/ostatnisluzby', function () {
-    return view('sluzby');
-});
+Route::view('/', 'homepage');
+Route::view('/dostupnost', 'dostupnost');
+Route::view('/bazen', 'bazen');
+Route::view('/wellness', 'wellness');
+Route::view('/cenik', 'cenik');
+Route::view('/ubytovani', 'ubytovani');
+Route::view('/sluzby', 'sluzby');
