3bce22976ecb06c27a1afcd9f2da6792aed46981
Michal Škoula <michal@skoula.com>
2025-04-17 12:13:53 +0200

    Cleanup routes

--- routes/web.php
+++ routes/web.php
@@ -2,48 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-Route::get('/', function () {
-    return view('homepage');
-});
-
-Route::get('/bazen', function () {
-    return view('bazen');
-});
-Route::get('/dostupnost', function () {
-    return view('dostupnost');
-});
-Route::get('/sauna', function () {
-    return view('sauna');
-});
-Route::get('/cenik', function () {
-    return view('cenik');
-});
-Route::get('/ubytovani', function () {
-    return view('ubytovani');
-});
-Route::get('/ostatnisluzby', function () {
-    return view('sluzby');
-});
-Route::get("/kontakty", function () {
-    return view('nastym');
-});
-Route::get("/clanek", function () {
-    return view('clanek');
-});
-Route::get("/komponenty", function () {
-    return view('komponenty');
-});
 Route::view('/', 'homepage')->name('index');
+Route::view('/kontakty', 'nastym');
+Route::view('/clanek', 'clanek');
+Route::view('/komponenty', 'komponenty');
 Route::view('/dostupnost', 'dostupnost')->name('dostupnost');
 Route::view('/bazen', 'bazen')->name('bazen');
 Route::view('/wellness', 'wellness')->name('wellness');
